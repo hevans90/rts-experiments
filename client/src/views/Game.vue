aba310a080472471f1ec1b8f8bbb4277d83cde63
Harri Evans <hevans9000@gmail.com>
2019-10-05 22:39:07 +0100

    feat: pixi canvas & rename about component to game

---
+++ client/src/views/Game.vue
@@ -0,0 +1,38 @@
+<template>
+  <canvas class="game" ref="renderCanvas"></canvas>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import * as PIXI from 'pixi.js';
+
+export default Vue.extend({
+  name: 'game',
+
+  mounted() {
+    // Determine the width and height of the renderer wrapper element.
+    const renderCanvas = this.$refs.renderCanvas as HTMLCanvasElement;
+    const width = renderCanvas.offsetWidth;
+    const height = renderCanvas.offsetHeight;
+
+    let game = new PIXI.Application({
+      width,
+      height,
+      view: renderCanvas,
+      backgroundColor: 0x1099bb,
+    });
+
+    game.renderer.view.style.position = 'absolute';
+    game.renderer.view.style.display = 'block';
+    game.renderer.autoResize = true;
+    game.renderer.resize(window.innerWidth, window.innerHeight);
+  },
+});
+</script>
+
+<style scoped>
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
