de945e69bc32c8deba0e9a4a74fcf31e0bb97b13
Harri Evans <hevans9000@gmail.com>
2019-10-15 15:57:59 +0200

    feat: add low res flat isometric tiles

---
+++ tilesets/iso_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="iso_tiles" tilewidth="16" tileheight="16" tilecount="624" columns="39">
+ <image source="iso_tiles.png" trans="ff00ff" width="628" height="256"/>
+</tileset>
