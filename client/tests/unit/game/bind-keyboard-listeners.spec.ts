a3da7df691662a99fcc6668757f226d120a160d4
Harri Evans <hevans9000@gmail.com>
2019-10-17 18:11:24 +0200

    test(client): finish tests for all arrow key events

--- client/tests/unit/game/bind-keyboard-listeners.spec.ts
+++ client/tests/unit/game/bind-keyboard-listeners.spec.ts
@@ -1,6 +1,7 @@
 import { bindKeyboardListeners } from '@/game/bind-keyboard-listeners';
 import * as keyboardApi from '@/game/utils/keyboard';
 import * as PIXI from 'pixi.js';
+jest.useFakeTimers();
 
 describe('bindKeyboardListeners', () => {
   const upArrowIndicator = new PIXI.Text('');
@@ -39,17 +40,162 @@ describe('bindKeyboardListeners', () => {
   });
 
   describe('post-registration', () => {
-    it('should correctly react to ArrowUp events', () => {
-      window.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp' }));
-      expect(mockVelocityChangeCallback).toHaveBeenCalled();
-      expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvely: 5 });
-      expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
-      mockVelocityChangeCallback.mockClear();
+    describe('ArrowUp', () => {
+      beforeEach(() =>
+        window.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp' })),
+      );
 
-      window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowUp' }));
-      expect(mockVelocityChangeCallback).toHaveBeenCalled();
-      expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ hardSetY: 0 });
-      expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      afterEach(() => {
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowUp' }));
+        mockVelocityChangeCallback.mockClear();
+      });
+
+      it('should call callback with a positive velocity delta in Y upon keydown', () => {
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvely: 5 });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call callback (with a small positive velocity delta in Y) every 17ms when held down', () => {
+        mockVelocityChangeCallback.mockClear();
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(2);
+        jest.advanceTimersByTime(68);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(6);
+        jest.advanceTimersByTime(1034);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(66);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvely: 2 });
+      });
+
+      it('should call callback with a hard set of 0 in Y velocity upon keydown', () => {
+        mockVelocityChangeCallback.mockClear();
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowUp' }));
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({
+          hardSetY: 0,
+        });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('ArrowDown', () => {
+      beforeEach(() =>
+        window.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 'ArrowDown' }),
+        ),
+      );
+
+      afterEach(() => {
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowDown' }));
+        mockVelocityChangeCallback.mockClear();
+      });
+
+      it('should call callback with a negative velocity delta in Y upon keydown', () => {
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvely: -5 });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call callback (with a small negative velocity delta in Y) every 17ms when held down', () => {
+        mockVelocityChangeCallback.mockClear();
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(2);
+        jest.advanceTimersByTime(68);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(6);
+        jest.advanceTimersByTime(1034);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(66);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvely: -2 });
+      });
+
+      it('should call callback with a hard set of 0 in Y velocity upon keydown', () => {
+        mockVelocityChangeCallback.mockClear();
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowDown' }));
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({
+          hardSetY: 0,
+        });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('ArrowLeft', () => {
+      beforeEach(() =>
+        window.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 'ArrowLeft' }),
+        ),
+      );
+
+      afterEach(() => {
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowLeft' }));
+        mockVelocityChangeCallback.mockClear();
+      });
+
+      it('should call callback with a positive velocity delta in X upon keydown', () => {
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvelx: 5 });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call callback (with a small positive velocity delta in X) every 17ms when held down', () => {
+        mockVelocityChangeCallback.mockClear();
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(2);
+        jest.advanceTimersByTime(68);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(6);
+        jest.advanceTimersByTime(1034);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(66);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvelx: 2 });
+      });
+
+      it('should call callback with a hard set of 0 in X velocity upon keydown', () => {
+        mockVelocityChangeCallback.mockClear();
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowLeft' }));
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({
+          hardSetX: 0,
+        });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('ArrowRight', () => {
+      beforeEach(() =>
+        window.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 'ArrowRight' }),
+        ),
+      );
+
+      afterEach(() => {
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }));
+        mockVelocityChangeCallback.mockClear();
+      });
+
+      it('should call callback with a negative velocity delta in X upon keydown', () => {
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvelx: -5 });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call callback (with a small negative velocity delta in X) every 17ms when held down', () => {
+        mockVelocityChangeCallback.mockClear();
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+        jest.advanceTimersByTime(17);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(2);
+        jest.advanceTimersByTime(68);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(6);
+        jest.advanceTimersByTime(1034);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(66);
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({ dvelx: -2 });
+      });
+
+      it('should call callback with a hard set of 0 in X velocity upon keydown', () => {
+        mockVelocityChangeCallback.mockClear();
+        window.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }));
+        expect(mockVelocityChangeCallback).toHaveBeenCalledWith({
+          hardSetX: 0,
+        });
+        expect(mockVelocityChangeCallback).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
